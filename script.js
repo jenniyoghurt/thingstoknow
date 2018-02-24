5649eed461f524a1aa596ede8df75de16206e964
jenniyoghurt <mia.snowdrop@gmail.com>
2018-02-24 02:02:22 +0100

    version 3.0

---
+++ script.js
@@ -0,0 +1,82 @@
+function hideAll(except) {
+    if ((except != "home") && ($("#homeBox").is(":visible"))) {
+        $( "#homeBox" ).slideToggle( "slow", "swing" );
+    }
+    if ((except != "profile") && ($("#profileBox").is(":visible"))) {
+        $( "#profileBox" ).slideToggle( "slow", "swing" );
+    }
+    if ((except != "favs") && ($("#favsBox").is(":visible"))) {
+        $( "#favsBox" ).slideToggle( "slow", "swing" );
+    }
+    if ((except != "notes") && ($("#notesBox").is(":visible"))) {
+        $( "#notesBox" ).slideToggle( "slow", "swing" );
+    }
+    if ((except != "asterisk") && ($("#asteriskBox").is(":visible"))) {
+        $( "#asteriskBox" ).slideToggle( "slow", "swing" );
+    }
+    if ((except != "art") && ($("#artBox").is(":visible"))) {
+        $( "#artBox" ).slideToggle( "slow", "swing" );
+    }
+    if ((except != "kofi") && ($("#kofiBox").is(":visible"))) {
+        $( "#kofiBox" ).slideToggle( "slow", "swing" );
+    }
+}
+    
+function makeVis(box) {
+    var str1 = "#"
+    var str2 = "Box"
+    var str = str1.concat(box.concat(str2))
+    if(!($(str).is(":visible"))) {
+        $(str).slideToggle( "slow", "swing" );
+    }
+}
+    
+function hideAndShow(box) {
+    hideAll(box)
+    makeVis(box)
+}
+
+function showHome() {
+    hideAndShow("home")
+}
+
+function showProfile() {
+    hideAndShow("profile")
+}
+
+function showFavs() {
+    hideAndShow("favs")
+}
+
+function showNotes() {
+    hideAndShow("notes")
+}
+
+function showAsterisk() {
+    hideAndShow("asterisk")
+}
+
+function showArt() {
+    hideAndShow("art")
+}
+
+function showKofi() {
+    hideAndShow("kofi")
+}
+
+
+
+
+function popup(card) {
+    var str = "#"
+    var str2 = str.concat(card)
+    var popup = str2.concat("Popup")
+    $(popup).slideDown( "slow", "swing");
+}
+
+function closePopup(card) {
+    var str = "#"
+    var str2 = str.concat(card)
+    var popup = str2.concat("Popup")
+    $(popup).slideUp( "slow", "swing");
+}
